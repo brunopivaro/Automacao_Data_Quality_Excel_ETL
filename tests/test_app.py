073bc5ae62eac126b5622587af4a351214039371
Bruno <brunopivaroc05@gmail.com>
2024-02-12 16:47:29 -0300

    Primeira Etapa do Projeto

---
+++ tests/test_app.py
@@ -0,0 +1,19 @@
+import selenium as sln
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from time import sleep
+
+driver = webdriver.Chrome()
+
+# Define um timeout implícito
+driver.set_page_load_timeout(5)  # 5 segundos
+
+# Vamos fazer uma tratativa de try-except de entrar na nossa página
+try:
+    driver.get("http://localhost:8501")
+    sleep(5)
+    print("Acessou a página com sucesso")
+except TimeoutException:
+    print("Tempo de carregamento da página excedeu o limite.")
+finally:
+    driver.quit()
