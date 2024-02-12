9dfc84eb44129ba294a6e0f8ad4124109f00f542
Bruno <brunopivaroc05@gmail.com>
2024-02-12 20:45:19 -0300

    feat: adicionar feature de testes unitários

--- src/app.py
+++ src/app.py
@@ -1,4 +1,12 @@
 import streamlit as st
 
-st.title('Primeiro Projeto')
+st.set_page_config(
+    page_title="Validador de schema excel"
+)
 
+st.title("Insira o seu excel para validação")
+
+arquivo = st.file_uploader("Carregue seu arquivo Excel aqui", type=["xlsx"])
+
+if arquivo is not None:
+    st.success("O schema do arquivo Excel está correto!")
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,19 +1,67 @@
 import selenium as sln
 from selenium import webdriver
-from selenium.common.exceptions import TimeoutException
 from time import sleep
+from selenium.webdriver.common.by import By
+import pytest
+import subprocess
+from selenium import webdriver
 
-driver = webdriver.Chrome()
+@pytest.fixture
+def driver():
+    # Iniciar o Streamlit em background
+    process = subprocess.Popen(["streamlit", "run", "src/app.py"])
 
-# Define um timeout implícito
-driver.set_page_load_timeout(5)  # 5 segundos
+    # Iniciar o WebDriver usando GeckoDriver
+    driver = webdriver.Chrome()
+    driver.set_page_load_timeout(10)
+    yield driver
 
-# Vamos fazer uma tratativa de try-except de entrar na nossa página
-try:
-    driver.get("http://localhost:8501")
-    sleep(5)
-    print("Acessou a página com sucesso")
-except TimeoutException:
-    print("Tempo de carregamento da página excedeu o limite.")
-finally:
+    # Fechar o WebDriver e o Streamlit após o teste
     driver.quit()
+    process.kill()
+
+def test_app_opens(driver):
+    # Verificar se a página abre
+    driver.get("http://localhost:8501")
+    Sleep(10)
+
+def test_check_title_is(driver):
+    # Verificar se a página abre
+    driver.get("http://localhost:8501")
+    # Verifica se o titulo de página é
+    sleep(10)
+    # Capturar o título da página
+    page_title = driver.title
+
+    # Verificar se o título da página é o esperado
+    expected_title = "Validador de schema excel"  # Substitua com o título real esperado
+    assert page_title == expected_title, f"O título da página era '{page_title}', mas esperava-se '{expected_title}'"
+
+def test_check_streamlit_h1(driver):
+    # Acessar a página do Streamlit
+    driver.get("http://localhost:8501")
+
+    # Aguardar para garantir que a página foi carregada
+    sleep(10)  # Espera 10 segundos
+
+    # Capturar o primeiro elemento <h1> da página
+    h1_element = driver.find_element(By.TAG_NAME, "h1")
+
+    # Verificar se o texto do elemento <h1> é o esperado
+    expected_text = "Insira o seu excel para validação"
+    assert h1_element.text == expected_text
+
+def test_check_usuario_pode_inserir_um_excel_e_receber_uma_mensagem(driver):
+    # Acessar a página do Streamlit
+    driver.get("http://localhost:8501")
+
+    # Aguardar para garantir que a página foi carregada
+    sleep(10)  # Espera 10 segundos
+
+    # Realizar o upload do arquivo de sucesso
+    success_file_path = os.path.abspath("data/arquivo_excel.xlsx")
+    driver.find_element(By.CSS_SELECTOR, 'input[type="file"]').send_keys(success_file_path)
+
+    # Aguardar a mensagem de sucesso
+    sleep(10)
+    assert "O schema do arquivo Excel está correto!" in driver.page_source
